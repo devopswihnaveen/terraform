690bcf54f960be67a3b20d2617675678bbecd9eb
naveen <naveenkumarvelanati@gmail.com>
2026-02-16 20:43:24 +0530

    Terraform count

--- count/variables.tf
+++ count/variables.tf
@@ -9,3 +9,35 @@ variable "zone_id" {
 variable "domain_name" {
     default = "amaravathi.today"
 }
+
+variable "sg_name" {
+    type = string
+    default = "allow-all"
+    description = "Security group name to attach to EC2 instance"
+}
+
+variable "cidr" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
+
+variable "ingress_from_port" {
+    default = 0
+}
+
+variable "ingress_to_port" {
+    default = 0
+}
+
+variable "egress_from_port" {
+    default = 0
+}
+
+variable "egress_to_port" {
+    default = 0
+}
+
+variable "protocol" {
+    type = string
+    default = "-1"
+}
