d012a31c5227a7145acb300b50ed068ccf86b39f
naveen <naveenkumarvelanati@gmail.com>
2026-02-16 20:25:56 +0530

    Terraform count and s3 locking

---
+++ count/ec2.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "terraform" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = var.ec2_tags
+}
+
+resource "aws_security_group" "allow_all" {
+    name = var.sg_name
+    egress {
+        from_port = var.egress_from_port
+        to_port = var.egress_to_port
+        protocol = "-1"
+        cidr_blocks = var.cidr
+    }
+    
+    ingressgress {
+        from_port = var.ingress_from_port
+        to_port = var.ingress_to_port
+        protocol = "-1"
+        cidr_blocks = var.cidr
+    }
+
+    tags = {
+        Name = "allow_all"
+    }
+}
---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend" ]
+}
+
+variable "zone_id" {
+    default = "Z02684311371WMKENMQPX"
+}
+
+variable "domain_name" {
+    default = "amaravathi.today"
+}
