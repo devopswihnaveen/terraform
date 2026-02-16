f6ec0112ccb353065660a4d88c8d3541eb7f3c5d
naveen <naveenkumarvelanati@gmail.com>
2026-02-16 21:15:43 +0530

    locals added

---
+++ locals/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.32.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-remote-state-naveen"
+    key = "remote-state-locals"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
