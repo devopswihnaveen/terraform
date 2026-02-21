da144c6650cdee27afaaf08d24e2a5146d37eb5a
naveen <naveenkumarvelanati@gmail.com>
2026-02-21 20:00:20 +0530

    terraform count

--- count/provider.tf
+++ count/provider.tf
@@ -5,14 +5,6 @@ terraform {
       version = "6.32.1"
     }
   }
-
-  backend "s3" {
-    bucket = "terraform-remote-state-naveen"
-    key = "remote-state-count"
-    region = "us-east-1"
-    use_lockfile = true
-    encrypt = true
-  }
 }
 
 provider "aws" {
