eee1fa57cfa876151507ac2cc0ad9b842c71bee6
naveen <naveenkumarvelanati@gmail.com>
2026-02-21 21:50:22 +0530

    EC2 and Route53 Setup done with private IPs

--- count/variables.tf
+++ count/variables.tf
@@ -2,3 +2,11 @@ variable "instances" {
     type = list
     default = ["mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend"]
 }
+
+variable "zone_id" {
+    default = "Z02684311371WMKENMQPX"
+}
+
+variable "domain_name" {
+    default = "amaravathi.today"
+}
