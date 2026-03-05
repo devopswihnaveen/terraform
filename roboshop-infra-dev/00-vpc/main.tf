0dfd289848c021002e051df0b15076dbcf93fb78
naveen <naveenkumarvelanati@gmail.com>
2026-03-05 07:36:48 +0530

    vpc changed

--- roboshop-infra-dev/00-vpc/main.tf
+++ roboshop-infra-dev/00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "../../terraform-aws-vpc"
+    source = "git::https://github.com/devopswihnaveen/terraform.git//terraform-aws-vpc?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = true
