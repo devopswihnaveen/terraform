0c501842fab09e04f678b57d83a2a713471611e4
naveen <naveenkumarvelanati@gmail.com>
2026-03-08 11:32:03 +0530

    changes added

---
+++ roboshop-infra-dev/40-databases/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z02684311371WMKENMQPX"
+}
+
+variable "domain_name" {
+    default = "amaravathi.today"
+}
